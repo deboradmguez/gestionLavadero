4438cc05e4540ba4d2a3d608bad3e38443882961
deboradmguez <debodominguez58@gmmail.com>
2024-06-14 15:30:45 -0300

    login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['idusuario'])) {
-    header("Location: login.html");
+    header("Location: index.html");
     exit();
 }
 ?><!DOCTYPE html>
@@ -22,7 +22,7 @@ if (!isset($_SESSION['idusuario'])) {
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
             <a class="navbar-logo ps-3" href="#">
-                <img src="logo-modified.png" alt="logo" ></a>
+                <img src="img\logo-modified.png" alt="logo" ></a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 ps-3" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
